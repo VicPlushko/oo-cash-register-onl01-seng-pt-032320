7f35b97452da5aac691aeef437f38bac498363d4
Victor Plushko <vicplushko33@gmail.com>
2020-04-16 05:41:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -37,7 +37,7 @@ class CashRegister
   
   def void_last_transaction
     @cart.pop()
-    @cart.each {|item| self.total += item[:price.to][:quantity] }
+    @cart.each {|item| self.total += item[:price][:quantity] }
     self.total
   end
 end
